f270640a31fe94a4951958d87fc671c41c8f38f4
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-06-29 22:15:17 +0200

    chore: adding initial front page

--- front-page.php
+++ front-page.php
@@ -1,71 +1,71 @@
 <?php
 
 defined( 'ABSPATH' ) || exit;
+get_header();
+do_action( 'wp_body_open' );
 
+$query_args = array(
+	'post_type'  => [ 'movie', 'event' ],
+	'meta_query' => array(
+		array(
+			'key'     => 'screening_date',
+			'value'   => time(),
+			'compare' => '>=',
+		)
+	),
+	'meta_key'   => 'screening_date',
+	'orderby'    => 'meta_value_num',
+	'order'      => 'ASC',
+);
 
-get_header('home');
-do_action('wp_body_open');
-
+$query = new WP_Query( $query_args );
 ?>
-<div id="page">
-    <section id="semesterprogram">
-        <div class="columns m-0">
-            <div class="column is-1 is-hidden-mobile">
-                <h2 class="text-rotated is-smallcaps font-ggl has-text-black is-size-1"><?= __('Unser Semesterprogramm') ?></h2>
-            </div>
-            <div class="column is-1 is-hidden-tablet">
-                <h2 class="font-ggl is-smallcaps has-text-black is-size-3"><?= __('Unser Semesterprogramm') ?></h2>
-            </div>
-            <div class="column">
-                <div class="fixed-grid has-1-cols-mobile has-2-cols-tablet has-3-cols-desktop has-3-cols-widescreen has-4-cols-fullhd">
-                    <div class="grid">
-                        <?php for ($i = 1; $i <= 10; $i++): 
-                            get_template_part('partials/tile', $i % 3 == 0 ? 'event': 'movie', array('counter' => $i));
-                        endfor; ?>
-                    </div>
+    <main class="pt-2 px-2 page-content">
+		<?php if ( $query->have_posts() ) : $query->the_post(); ?>
+            <div class="next-movie">
+                <hr class="separator"/>
+                <div class="next-movie-header">
+                    <p><?= esc_html__( 'Next Screening' ) ?></p>
+                    <time datetime="<?= date( "Y-m-d H:i", rwmb_meta( 'screening_date' ) ) ?>"><p
+                                class="is-size-6 m-0 p-0"><?= date( "d.m.Y | H:i", rwmb_meta( 'screening_date' ) ) ?></p>
+                    </time>
                 </div>
-
-
-            </div>
-        </div>
-    </section>
-
-    <?php //todo: check displayed special programs from theme_mod and display in <section> ?>
-    <section id="special-program-x" class="full-width-section"
-        style="background-image: url('<?= get_stylesheet_directory_uri() ?>/assets/img/special-program.svg'), linear-gradient(to bottom,  #356aca,  #356aca); background-size: cover; background-position: center;">
-        <div class="columns">
-            <div class="column is-1 is-hidden-mobile">
-                <h2 class="text-rotated is-smallcaps font-ggl has-text-white is-size-1"> <?= __('Special Program X') ?></h2>
-            </div>
-            <div class="column is-1 is-hidden-tablet">
-                <h2 class="is-smallcaps font-ggl has-text-white is-size-3"> <?= __('Special Program X') ?></h2>
-            </div>
-            <div class="column">
-                <p class="has-text-white has-text-weight-semibold is-size-4 pb-4">
-                    <?= __('Dies ist die Beschreibung des Sonderprogramms') ?>
-                </p>
-                <div class="fixed-grid has-1-cols-mobile has-2-cols-tablet has-3-cols-desktop has-3-cols-widescreen has-4-cols-fullhd">
-                    <div class="grid">
-                        <?php for ($i = 1; $i <= 10; $i++): 
-                            get_template_part('partials/tile', 'movie', array('counter' => $i));
-                        endfor; ?>
-                    </div>
+                <hr class="separator"/>
+                <h2 class="title is-uppercase next-movie-title"><?= rwmb_meta( 'german_title' ) ?></h2>
+                <hr class="separator"/>
+                <div>
+                <figure class="image is-hidden-tablet is-4by5 movie-image">
+                    <img src="<?= get_the_post_thumbnail_url( size: 'full' ) ?>"/>
+                </figure>
+                <figure class="image is-hidden-mobile is-16by9 movie-image">
+                    <img src="<?= get_the_post_thumbnail_url( size: 'full' ) ?>"/>
+                </figure>
                 </div>
+                <div>
+                    <a class="button is-outlined is-black is-fullwidth mt-2" href="<?= get_the_permalink() ?>">
+                        <p class="is-size-4 is-uppercase py-1"><?= esc_html__( 'To the movie' ) ?></p>
+                    </a>
+                </div>
+                <hr class="separator"/>
             </div>
+		<?php endif; ?>
+        <!-- Display of next movie will be setup later -->
+        <h1 class="title is-uppercase"><?= esc_html__('Our Semester Program') ?></h1>
+        <div class="movie-list">
+			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                <a class="is-flex is-justify-content-space-between is-align-items-center" href="<?= get_permalink() ?>">
+                    <div>
+                        <time datetime="<?= date( "Y-m-d H:i", rwmb_meta( 'screening_date' ) ) ?>"><p
+                                    class="is-size-6 m-0 p-0"><?= date( "d.m.Y | H:i", rwmb_meta( 'screening_date' ) ) ?></p>
+                        </time>
+                        <p class="is-size-5 has-text-weight-bold is-uppercase"><?= rwmb_meta( 'german_title' ) ?></p>
+                    </div>
+                    <span class="icon">
+                        <span class="material-symbols">arrow_forward_ios</span>
+                    </span>
+                </a>
+			<?php endwhile; ?>
         </div>
-    </section>
+    </main>
 
-    <section id="who-we-are" class="full-width-section" style="background-color: white;">
-        <div class="columns p-0"
-            style="width: var(--page-width); margin: auto; background-color: var(--bulma-primary);">
-            <div class="column is-8">
-                <h2 class="font-ggl is-size-3 has-text-black is-uppercase">
-                    <?= __('Who is the GEGENLICHT') //todo: replace with theme_mod ?>
-                </h2>
-            </div>
-        </div>
-    </section>
-
-
-</div>
-<?php //get_footer(); ?>
+<?php get_footer(); ?>
---
+++ page.php
@@ -0,0 +1,11 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+get_header();
+do_action('wp_body_open');
+?>
+	<div id="content">
+		<h1 class="title">This is a page playground</h1>
+	</div>
+
+<?php //get_footer(); ?>
