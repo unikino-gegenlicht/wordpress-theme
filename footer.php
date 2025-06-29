40515181b88af7e56b94880f2f1fde5c2504d0bf
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-06-29 22:13:23 +0200

    chore: adding footer and header

--- footer.php
+++ footer.php
@@ -6,12 +6,42 @@
 defined('ABSPATH') || exit;
 ?>
 
-<footer class="footer">
-    <section class="hero is-primary">
-        <div class="hero-body">
-            <p>TEST 123</p>
-        </div>
-    </section>
+<footer class="footer has-text-primary">
+    <p class="is-size-3 font-ggl is-uppercase has-text-primary">
+	    <?= str_replace( ' ', '<br class="is-hidden-tablet"/> ', get_bloginfo( 'name' ) ) ?>
+    </p>
+    <hr class="separator is-primary"/>
+    <?php if (get_theme_mod('instagram_username') || get_theme_mod('mastodon_url') || get_theme_mod('letterboxd_username')): ?>
+    <div class="is-flex is-align-items-center">
+        <?php if (get_theme_mod('instagram_username')): ?>
+        <a href="https://instagram.com/<?= get_theme_mod('instagram_username') ?>" class="has-text-primary"><span class="is-underlined">Instagram</span><span class="material-symbols">open_in_new</span> </a>
+        <?php endif; ?>
+	    <?php if (get_theme_mod('mastodon_url')): ?>
+            <a rel="me" href="<?= get_theme_mod('mastodon_url') ?>" class="has-text-primary"><span class="is-underlined">Mastodon</span><span class="material-symbols">open_in_new</span> </a>
+	    <?php endif; ?>
+	    <?php if (get_theme_mod('letterboxd_username')): ?>
+            <a rel="me" href="https://letterboxd.com/<?= get_theme_mod('letterboxd_username') ?>" class="has-text-primary"><span class="is-underlined">Letterboxd</span><span class="material-symbols">open_in_new</span> </a>
+	    <?php endif; ?>
+    </div>
+    <hr class="separator is-primary"/>
+    <?php endif; ?>
+    <address class="has-text-primary my-2">
+        <?= get_theme_mod('address_line_1') ?>
+        <?= get_theme_mod('address_line_2') ?>
+        <?= get_theme_mod('address_street') ?>
+        <?= get_theme_mod('address_zip_code') ?> <?= get_theme_mod('address_city') ?>
+    </address>
+    <hr class="separator is-primary"/>
+    <div class="has-text-primary my-2">
+        <h5 class="has-text-weight-semibold"><?= esc_html__('Youth Protection Officer') ?></h5>
+        <a href="mailto:<?= get_theme_mod('ypo_email') ?>"><?= get_theme_mod('ypo_name') ?></a>
+        <p><?= str_replace("@", " (at) ", get_theme_mod('ypo_email')) ?></p>
+    </div>
+    <div class="is-flex is-align-items-center my-2 has-text-primary footer-links is-flex-wrap-wrap">
+        <a class="has-text-primary" href=""><?= esc_html__('Impress') ?></a>
+        <a href=""><?= esc_html__('Privacy') ?></a>
+        <a href=""><?= esc_html__('Contact') ?></a>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- header.php
+++ header.php
@@ -1,65 +1,108 @@
 <?php
 /**
  * The HTML header for the theme
- * 
- * This file renders the HTML header block for the wordpress theme which 
+ *
+ * This file renders the HTML header block for the wordpress theme which
  * includes preloading/prefetching stylesheets, common assets and fonts
  */
 defined( 'ABSPATH' ) || exit;
 
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> data-theme="">
 <head>
-    <title><?php bloginfo('name') ?></title>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo( 'name' ) ?></title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-    <?php if ( defined('WP_DEBUG') && WP_DEBUG ): ?>
-    <link rel="preload" href="<?= get_stylesheet_directory_uri()?>/style.css" as="style">
-    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri()?>/style.css" as="style">
-    <?php else: ?>
-    <link rel="preload" href="<?= get_stylesheet_directory_uri()?>/style.min.css" as="style">
-    <link rel="stylesheet" href="<?= get_stylesheet_directory_uri()?>/style.min.css" as="style">
-    <?php endif; ?>
-    <link rel="preload" href="<?= get_stylesheet_directory_uri()?>/assets/fonts/gegenlicht.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="<?= get_stylesheet_directory_uri()?>/assets/fonts/inter.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="<?= get_stylesheet_directory_uri()?>/assets/fonts/inter_italic.woff2" as="font" type="font/woff2" crossorigin>
-    <?php if (is_user_logged_in()): ?>
-    <link rel="preload" href="<?= get_stylesheet_directory_uri()?>/assets/icons/logout.svg" as="image" type="image/svg+xml" crossorigin>
-    <?php else: ?>
-    <link rel="preload" href="<?= get_stylesheet_directory_uri()?>/assets/icons/account.svg" as="image" type="image/svg+xml" crossorigin>
-    <?php endif; ?>
-    <?php wp_head(); ?>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php if ( defined( 'WP_DEBUG' ) && WP_DEBUG ): ?>
+        <link rel="preload" href="<?= get_stylesheet_directory_uri() ?>/style.css" as="style">
+        <link rel="stylesheet" href="<?= get_stylesheet_directory_uri() ?>/style.css" as="style">
+	<?php else: ?>
+        <link rel="preload" href="<?= get_stylesheet_directory_uri() ?>/style.min.css" as="style">
+        <link rel="stylesheet" href="<?= get_stylesheet_directory_uri() ?>/style.min.css" as="style">
+	<?php endif; ?>
+    <link rel="preload" href="<?= get_stylesheet_directory_uri() ?>/assets/fonts/gegenlicht.woff2" as="font"
+          type="font/woff2" crossorigin>
+    <link rel="preload" href="<?= get_stylesheet_directory_uri() ?>/assets/fonts/inter.woff2" as="font"
+          type="font/woff2" crossorigin>
+    <link rel="preload" href="<?= get_stylesheet_directory_uri() ?>/assets/fonts/inter_italic.woff2" as="font"
+          type="font/woff2" crossorigin>
+    <script src="<?= get_stylesheet_directory_uri() ?>/assets/js/darkmode-toggle.js"></script>
+    <script src="<?= get_stylesheet_directory_uri() ?>/assets/js/menu-toggle.js"></script>
+	<?php wp_head(); ?>
 </head>
+<body>
 <header>
-    <nav class="navbar is-spaced" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="<?= get_home_url(scheme: 'https') ?>">
-                <?php
-                    $logoId = get_theme_mod('custom_logo');
-                    $alternativeDescription = get_post_meta($logoId, '_wp_attachment_image_alt', true);
-                    $logoUrl = wp_get_attachment_image_url($logoId, 'full')
-                ?>
-                <img src="<?= $logoUrl ?>" alt="<?= $alternativeDescription ?>"/>
+    <nav class="navbar is-spaced is-primary" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand px-2">
+            <a class="navbar-item p-0" href="<?= get_home_url( scheme: 'https' ) ?>">
+				<?php
+				if ( get_theme_mod( 'header_logo' ) != false ):
+					$logoId = get_theme_mod( 'header_logo' );
+					$alternativeDescription = get_post_meta( $logoId, '_wp_attachment_image_alt', true );
+					$logoUrl = wp_get_attachment_image_url( $logoId, 'full' )
+					?>
+                    <img style="height: 56px" src="<?= $logoUrl ?>" alt="<?= $alternativeDescription ?>"/>
+				<?php else: ?>
+                    <p class="title has-text-black">
+						<?= str_replace( ' ', '<br/>', get_bloginfo( 'name' ) ) ?>
+                    </p>
+				<?php endif; ?>
+            </a>
+
+            <a role="button" id="burger" class="navbar-burger" aria-label="menu" aria-expanded="false"
+               style="color:black;"
+               onclick="toggleMenu()">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
             </a>
         </div>
-        <div class="navbar-menu">
+        <div class="navbar-menu" id="menu">
             <div class="navbar-end">
                 <!-- Generated Menu by WordPress -->
-                <?php
-                $navigationItems = wp_get_nav_menu_items('navigation');
-                for ( $i = 0; $i < count($navigationItems); $i ++ ):
-                    $navigationItem = $navigationItems[$i];
-                ?>
-                    <a class="navbar-item" href="<?= $navigationItem->url ?>"><?= $i ?>. <span class="has-text-weight-semibold"><?= $navigationItem->title ?></span></a>
-                <?php endfor; ?>
-                <?php if (is_user_logged_in()): ?>
-                    <a class="navbar-item" href="<?= wp_logout_url(get_page_uri()) ?>"><span class="icon is-large"><img src="<?=get_stylesheet_directory_uri()?>/assets/icons/logout.svg"</span></a>
-                <?php else: ?>
-                    <a class="navbar-item" href="<?= wp_login_url(get_page_uri()) ?>"><span class="icon is-large"><img src="<?=get_stylesheet_directory_uri()?>/assets/icons/account.svg"</span></a>
-                <?php endif ?>
+				<?php
+				$navigationItems = wp_get_nav_menu_items( wp_get_nav_menu_name( 'navigation-menu' ) );
+				if ( $navigationItems ):
+					for ( $i = 0; $i < count( $navigationItems ); $i ++ ):
+						$navigationItem = $navigationItems[ $i ];
+
+						$num = '';
+						if ( ( $i + 1 ) < 9 ) {
+							$num = '0' . $i + 1;
+						} else {
+							$num = $i + 1;
+						}
+						global $wp;
+						$currentPage = $navigationItem->url == home_url( $wp->request );
+						?>
+                        <!-- <?= $navigationItem->url ?> -->
+                        <!-- <?= home_url( $wp->request ) ?> -->
+                        <a class="navbar-item is-size-5 is-tab <?= $currentPage ? 'is-active' : '' ?>"
+                           href="<?= $navigationItem->url ?>"><?= $num ?> <span
+                                    class="is-size-5 has-text-weight-semibold is-uppercase "><?= $navigationItem->title ?></span></a>
+					<?php endfor; ?>
+				<?php endif; ?>
+                <hr>
+				<?php if ( is_user_logged_in() ): ?>
+                    <a class="navbar-item" href="<?= wp_logout_url( is_home() ? home_url() : get_page_uri() ) ?>">
+                        <span class="icon-text">
+                        <span
+                                class="icon"><span class="material-symbols">logout</span></span>
+                        <span class="is-size-5 has-text-weight-semibold is-uppercase is-hidden-desktop"><?= esc_html__( 'Logout', 'gegenlicht' ) ?></span></span>
+                    </a>
+				<?php else: ?>
+                    <a class="navbar-item" href="<?= wp_login_url( is_home() ? home_url() : get_page_uri() ) ?>">
+                        <span class="icon-text">
+                        <span
+                                class="icon"><span class="material-symbols">login</span></span>
+                        <span class="is-size-5 has-text-weight-semibold is-uppercase is-hidden-desktop"><?= esc_html__( 'Login', 'gegenlicht' ) ?></span></span>
+                    </a>
+				<?php endif ?>
             </div>
         </div>
     </nav>
 </header>
+</body>
