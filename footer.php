7654dfcfdcca6ecfdb988102ba60c07af8450f6c
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-07-06 10:20:15 +0200

    chore: updating login and logout redirects generation

--- footer.php
+++ footer.php
@@ -3,41 +3,49 @@
 /**
  * The footer of all pages
  */
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 ?>
 
 <footer class="footer has-text-primary">
     <p class="is-size-3 font-ggl is-uppercase has-text-primary">
-	    <?= str_replace( ' ', '<br class="is-hidden-tablet"/> ', get_bloginfo( 'name' ) ) ?>
+		<?= str_replace( ' ', '<br class="is-hidden-tablet"/> ', get_bloginfo( 'name' ) ) ?>
     </p>
     <hr class="separator is-primary"/>
-    <?php if (get_theme_mod('instagram_username') || get_theme_mod('mastodon_url') || get_theme_mod('letterboxd_username')): ?>
-    <div class="is-flex is-align-items-center my-2 has-text-primary footer-links is-flex-wrap-wrap">
-        <?php if (get_theme_mod('instagram_username')): ?>
-            <a href="https://instagram.com/<?= get_theme_mod('instagram_username') ?>" class="has-text-primary"><span class="icon-text"><span class="is-underlined">Instagram</span><span class="material-symbols ml-1">open_in_new</span></span> </a>
-        <?php endif; ?>
-	    <?php if (get_theme_mod('mastodon_url')): ?>
-            <a href="<?= get_theme_mod('mastodon_url') ?>" class="has-text-primary"><span class="icon-text"><span class="is-underlined">Mastodon</span><span class="material-symbols ml-1">open_in_new</span></span> </a>
-	    <?php endif; ?>
-	    <?php if (get_theme_mod('letterboxd_username')): ?>
-            <a href="https://letterboxd.com/<?= get_theme_mod('letterboxd_username') ?>" class="has-text-primary"><span class="icon-text"><span class="is-underlined">Letterboxd</span><span class="material-symbols ml-1">open_in_new</span></span> </a>
-	    <?php endif; ?>
-    </div>
-    <hr class="separator is-primary"/>
-    <?php endif; ?>
+	<?php if ( get_theme_mod( 'instagram_username' ) || get_theme_mod( 'mastodon_url' ) || get_theme_mod( 'letterboxd_username' ) ): ?>
+        <div class="is-flex is-align-items-center my-2 links are-primary is-flex-wrap-wrap">
+			<?php if ( get_theme_mod( 'instagram_username' ) ): ?>
+                <a href="https://instagram.com/<?= get_theme_mod( 'instagram_username' ) ?>"><span
+                            class="icon-text"><span class="is-underlined">Instagram</span><span
+                                class="material-symbols ml-1">open_in_new</span></span> </a>
+			<?php endif; ?>
+			<?php if ( get_theme_mod( 'mastodon_url' ) ): ?>
+                <a href="<?= get_theme_mod( 'mastodon_url' ) ?>"><span class="icon-text"><span
+                                class="is-underlined">Mastodon</span><span
+                                class="material-symbols ml-1">open_in_new</span></span> </a>
+			<?php endif; ?>
+			<?php if ( get_theme_mod( 'letterboxd_username' ) ): ?>
+                <a href="https://letterboxd.com/<?= get_theme_mod( 'letterboxd_username' ) ?>"><span
+                            class="icon-text"><span class="is-underlined">Letterboxd</span><span
+                                class="material-symbols ml-1">open_in_new</span></span> </a>
+			<?php endif; ?>
+        </div>
+        <hr class="separator is-primary"/>
+	<?php endif; ?>
     <address class="has-text-primary my-2">
-        <?= nl2br(get_theme_mod('address') )  ?>
+		<?= nl2br( get_theme_mod( 'address' ) ) ?>
     </address>
     <hr class="separator is-primary"/>
     <div class="has-text-primary my-2">
-        <h5 class="has-text-weight-semibold"><?= esc_html__('Youth Protection Officer') ?></h5>
-        <a class="has-text-primary" href="mailto:<?= get_theme_mod('ypo_email') ?>"><?= get_theme_mod('ypo_name') ?></a>
-        <p><?= str_replace("@", " (at) ", get_theme_mod('ypo_email')) ?></p>
+        <h5 class="has-text-weight-semibold"><?= esc_html__( 'Youth Protection Officer' ) ?></h5>
+        <a class="has-text-primary"
+           href="mailto:<?= get_theme_mod( 'ypo_email' ) ?>"><?= get_theme_mod( 'ypo_name' ) ?></a>
+        <p><?= str_replace( "@", " (at) ", get_theme_mod( 'ypo_email' ) ) ?></p>
     </div>
-        <div class="is-flex is-align-items-center my-2 has-text-primary footer-links is-flex-wrap-wrap">
-        <a class="has-text-primary" href=""><?= esc_html__('Impress') ?></a>
-        <a class="has-text-primary" href="<?= get_privacy_policy_url() ?>" rel="privacy-policy"><?= esc_html__('Privacy Policy') ?></a>
-        <a class="has-text-primary" href=""><?= esc_html__('Contact') ?></a>
+    <div class="is-flex is-align-items-center my-2 has-text-primary links are-primary is-flex-wrap-wrap">
+        <a class="" href=""><?= esc_html__( 'Impress' ) ?></a>
+        <a class="" href="<?= get_privacy_policy_url() ?>"
+           rel="privacy-policy"><?= esc_html__( 'Privacy Policy' ) ?></a>
+        <a class="" href=""><?= esc_html__( 'Contact' ) ?></a>
     </div>
 </footer>
 <?php wp_footer(); ?>
