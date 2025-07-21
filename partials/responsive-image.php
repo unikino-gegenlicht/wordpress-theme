5c6365cccc01c1ca27a6109ef66dafb9290cbd95
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-07-22 01:03:50 +0200

    chore: allowing loading of images by post id in responsive image partial

--- partials/responsive-image.php
+++ partials/responsive-image.php
@@ -1,12 +1,25 @@
 <?php
-$imageUrl = $args['image_url'] ?? wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'large' ) ?: 'https://picsum.photos/1600/900/';
-$alternativeDescription = attachment_url_to_postid( $imageUrl ) != 0 ? get_post_meta(attachment_url_to_postid( $imageUrl ), '_wp_attachment_image_alt', true ) : '';
+
+$postID = ($args['post-id'] ?? null);
+$lazyLoad = (bool) ($args['lazyload'] ?? false);
+$style = ($args['style'] ?? "");
+
+if (isset($post) && $postID != null):
+    $desktopImageUrl = get_the_post_thumbnail_url( $postID, 'desktop' ) ?: wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'desktop' ) ?: wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'desktop' ) ?: "";
+    $mobileImageUrl = get_the_post_thumbnail_url( $postID, 'mobile' ) ?: wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'mobile' ) ?: wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'mobile' ) ?: "";
+	$alternativeDescription = (($imageID = attachment_url_to_postid($desktopImageUrl)) == 0) ? "" : get_post_meta($imageID, '_wp_attachment_image_alt', true);
+else:
+    $desktopImageUrl = $args['image_url'] ?? wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'desktop' ) ?: "";
+    $mobileImageUrl = $args['mobile_image_url'] ?? $args['image_url'] ?? wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'mobile' ) ?: "";
+    $alternativeDescription = $args['alternative_description'] ?? "";
+endif;
+
 ?>
 <figure class="image is-hidden-tablet is-4by5 movie-image mt-4">
     <img alt="<?= $alternativeDescription ?>" fetchpriority="<?= $args['fetch-priority'] ?? 'normal' ?>"
-         src="<?= $imageUrl ?>"/>
+         src="<?= $mobileImageUrl ?>" style="<?= esc_attr($style) ?>"/>
 </figure>
 <figure class="image is-hidden-mobile <?= ($args['disable16by9'] ?? false) ? '' : 'is-16by9' ?> movie-image mt-4">
     <img alt="<?= $alternativeDescription ?>" fetchpriority="<?= $args['fetch-priority'] ?? 'normal' ?>"
-         src="<?= $imageUrl ?>"/>
+         src="<?= $desktopImageUrl ?>" style="<?= esc_attr($style) ?>"/>
 </figure>
