2efb432194feb20518d4d84f94f5899ec554ec48
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-07-21 21:15:24 +0200

    chore: updating contact-block to be partial

--- partials/contact-block.php
+++ partials/contact-block.php
@@ -1,72 +1,89 @@
 <?php
-function ggl_do_contact_shortcode( array $atts, $content = null, $tag = '' ) {
+$fullwidth       = (bool) ( $args['fullwidth'] ?? false );
+$showPhoneNumber = (bool) ( $args['showPhoneNumber'] ?? false );
+$showEmail       = (bool) ( $args['showEmail'] ?? true );
+$showInstagram   = (bool) ( $args['showInstagram'] ?? true );
+$showMastodon    = (bool) ( $args['showMastodon'] ?? true );
+$showLetterboxd  = (bool) ( $args['showLetterboxd'] ?? false );
 
-	$atts = array_change_key_case( $atts );
+$phoneNumber        = (string) ( $args['phoneNumber'] ?? get_theme_mod( 'contact_info' )["phoneNumber"] ?? "" );
+$emailAddress       = (string) ( $args['emailAddress'] ?? get_theme_mod( 'contact_info' )["emailAddress"] ?? "" );
+$instagramUsername  = (string) ( $args['instagramUsername'] ?? get_theme_mod( 'social_medias' )["instagram"] ?? "" );
+$mastodonUsername   = (string) ( $args['mastodonURL'] ?? ( ( get_theme_mod( 'social_medias' )["mastodonURL"] ?? null ) != null ? explode( "/", get_theme_mod( 'socialMedias' )["mastodonURL"] )[3] . "@" . explode( "/", get_theme_mod( 'socialMedias' )["mastodonURL"] )[2] : "" ) );
+$letterboxdUsername = (string) ( $args['letterboxdUsername'] ?? get_theme_mod( 'social_medias' )["letterboxd"] ?? "" );
 
-	$options = shortcode_atts( array(
-			'fullwidth'          => false,
-			'display_phone'      => false,
-			'display_email'      => true,
-			'display_instagram'  => true,
-			'display_mastodon'   => true,
-			'display_letterboxd' => false,
-			'phone_number'       => get_theme_mod( 'contact_telephone_number' ),
-			'email_address'      => get_theme_mod( 'contact_email_address' ),
-			'mastodon_url'       => get_theme_mod( 'mastodon_url' ),
-			'instagram_username' => get_theme_mod( 'instagram_username' ),
-		), $atts, $tag );
+$backgroundColorDark = (string) ( $args['colors']['background']['dark'] ?? get_theme_mod( 'contact_partial' )['background']['dark'] ?? 'black' );
+$backgroundColor     = (string) ( $args['colors']['background']['light'] ?? get_theme_mod( 'contact_partial' )['background']['light'] ?? 'black' );
 
+$textColorDark = (string) ( $args['colors']['text']['dark'] ?? get_theme_mod( 'contact_partial' )['text']['dark'] ?? '#ffdd00' );
+$textColor     = (string) ( $args['colors']['text']['light'] ?? get_theme_mod( 'contact_partial' )['text']['light'] ?? '#ffdd00' );
+?>
 
-	$mastodonProfileUrl = $options['mastodon_url'];
+<style>
+    .contact-block {
+        --bulma-body-background-color: <?= $backgroundColor ?>;
+        --bulma-body-color: <?= $textColor ?>;
 
-	$mastodonProfileParts = explode( "/", $mastodonProfileUrl );
-	$mastodonProfileName  = $mastodonProfileParts[3] . "@" . $mastodonProfileParts[2];
+        --bulma-content-table-cell-padding: 0.25rem 0;
+        --bulma-content-table-cell-border: none;
 
-	?>
-    <div class="has-background-black has-text-primary py-1">
-        <div class="limited-width-block">
-            <h2 class="is-size-3 pl-2 is-uppercase"><?= esc_html__( 'Contact', 'gegenlicht' ) ?></h2>
-            <div class="fixed-grid has-7-cols pl-2">
-                <div class="grid is-row-gap-0 is-column-gap-4">
-					<?php if ( $options['display_phone'] ): ?>
-                        <div class="cell is-lowercase">
-							<?= esc_html__( 'Telephone', 'gegenlicht' ) ?>
-                        </div>
-                        <div class="cell is-col-span-6">
-							<?= $options['phone_number'] ?>
-                        </div>
-					<?php endif; ?>
-					<?php if ( $options['display_email'] ): ?>
-                        <div class="cell">
-                            mail:
-                        </div>
-                        <div class="cell is-col-span-6">
-							<?= $options['email_address'] ?>
-                        </div>
-					<?php endif; ?>
-					<?php if ( $options['display_mastodon'] ): ?>
+        .table {
+            --bulma-table-background-color: var(--bulma-body-background-color);
+            --bulma-table-color: var(--bulma-body-color);
+        }
 
-                        <div class="cell">
-                            mastodon:
-                        </div>
-                        <div class="cell is-col-span-6">
-							<?= $mastodonProfileName ?>
-                        </div>
-					<?php endif; ?>
-					<?php if ( $options['display_instagram'] ): ?>
-                        <div class="cell">
-                            instagram:
-                        </div>
-                        <div class="cell is-col-span-6">
-							<?= $options['instagram_username'] ?>
-                        </div>
-					<?php endif; ?>
-                </div>
-            </div>
-            <p class="font-ggl is-uppercase pl-2 pb-2"><?= esc_html__( 'or just reach out to us at one of our screenings', 'gegenlicht' ) ?></p>
-        </div>
+        background-color: var(--bulma-body-background-color);
+        color: var(--bulma-body-color);
 
+        padding: 0.5rem 0;
+        margin: 1rem 0;
+
+        .info {
+            max-width: var(--max-content-width) !important;
+            margin: 0 auto !important;
+            padding: 0 var(--content-padding) !important;
+        }
+    }
+</style>
+<article class="contact-block content">
+    <div class="info">
+        <h2 class="is-size-3 is-uppercase pt-4">
+			<?= esc_html__( "Contact Us", "gegenlicht" ) ?>
+        </h2>
+        <table class="table is-fullwidth">
+			<?php if ( $showPhoneNumber ) : ?>
+                <tr>
+                    <td><?= esc_html__( "Telephone", "gegenlicht" ) ?></td>
+                    <td><?= $phoneNumber ?></td>
+                </tr>
+			<?php endif; ?>
+			<?php if ( $showEmail && !empty($emailAddress) ) : ?>
+                <tr>
+                    <td><?= esc_html__( "e-mail", "gegenlicht" ) ?></td>
+                    <td><?= preg_replace( "/@/", " (at) ", $emailAddress ) ?></td>
+                </tr>
+			<?php endif; ?>
+	        <?php if ( $showInstagram && !empty($instagramUsername) ) : ?>
+                <tr>
+                    <td><?= esc_html__( "Instagram", "gegenlicht" ) ?></td>
+                    <td><?= $instagramUsername ?></td>
+                </tr>
+	        <?php endif; ?>
+	        <?php if ( $showMastodon && !empty($mastodonUsername) ) : ?>
+                <tr>
+                    <td><?= esc_html__( "Mastodon", "gegenlicht" ) ?></td>
+                    <td><?= $mastodonUsername ?></td>
+                </tr>
+	        <?php endif; ?>
+	        <?php if ( $showLetterboxd && !empty($letterboxdUsername) ) : ?>
+                <tr>
+                    <td><?= esc_html__( "Letterboxd", "gegenlicht" ) ?></td>
+                    <td><?= $letterboxdUsername ?></td>
+                </tr>
+	        <?php endif; ?>
+        </table>
+        <p class="is-size-5 is-uppercase font-ggl pb-4">
+            <?= esc_html__("Or just chat us up at one of our screenings", "gegenlicht" ) ?>
+        </p>
     </div>
-
-	<?php
-}
+</article>
