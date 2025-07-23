02f4cfaae2ca8e9b9ad49520a26a8a6eefe9341f
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-07-23 11:34:02 +0200

    chore: adding page handler for privacy policy

--- privacy-policy.php
+++ privacy-policy.php
@@ -10,8 +10,9 @@ get_header();
 	<header>
 		<h1><?= esc_html__('Privacy Policy') ?></h1>
 	</header>
-	<div class="pt-3 content privacy-policy">
+	<div class="pt-3 content privacy-policy pb-5">
 		<?=	apply_filters('the_content', get_the_content()) ?>
 	</div>
 </main>
+<?php get_footer(); ?>
 
