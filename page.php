4a79de2b4826702ba8d02de8cbcdc90dc8c72925
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-07-23 11:39:05 +0200

    chore: adding plain page template

--- page.php
+++ page.php
@@ -8,6 +8,10 @@ get_header();
 do_action('wp_body_open');
 ?>
 <article class="page-content content">
-    <?= get_the_content() ?>
+    <header>
+        <h1><?= get_the_title() ?></h1>
+    </header>
+
+    <?= apply_filters('the_content', get_the_content()) ?>
 </article>
 <?php get_footer(); ?>
