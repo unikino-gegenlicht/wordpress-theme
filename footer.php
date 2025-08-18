21bb6d2384a6e0bd1bd32efd6d11124011904713
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-18 13:37:06 +0200

    fix: increasing margins around footer entries

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@ defined( 'ABSPATH' ) || exit;
     </p>
     <hr class="separator is-primary"/>
 	<?php if ( ! empty( get_theme_mod( 'displayed_social_medias' ) ?? [] ) ): ?>
-        <div class="is-flex is-align-items-center my-2 are-primary is-flex-wrap-wrap links">
+        <div class="is-flex is-align-items-center my-3 are-primary is-flex-wrap-wrap links">
 			<?php foreach ( get_theme_mod( 'displayed_social_medias' ) as $socialMedia => $entry ): ?>
                 <a rel="me" href="<?= get_theme_mod( "social_medias" )[ $entry ] ?>"><span
                             class="icon-text"><span class="is-underlined"><?= ucfirst( $entry ) ?></span><span
@@ -17,10 +17,10 @@ defined( 'ABSPATH' ) || exit;
         </div>
         <hr class="separator is-primary"/>
 	<?php endif; ?>
-    <div class="has-text-primary my-2 is-flex is-align-items-top is-flex-wrap-wrap is-justify-content-space-evenly is-row-gap-2.5">
+    <div class="has-text-primary my-5 is-flex is-align-items-top is-flex-wrap-wrap is-justify-content-space-evenly is-row-gap-2.5">
         <?php if (in_array("postal", get_theme_mod("displayed_addresses") ?? [])): ?>
         <div class="is-flex-grow-1">
-            <h5 class="has-text-weight-semibold is-size-6 is-underlined"><?= esc_html__( 'Postal Address', 'gegenlicht' ) ?></h5>
+            <h5 class="has-text-weight-semibold no-separator is-size-6 is-underlined"><?= esc_html__( 'Postal Address', 'gegenlicht' ) ?></h5>
             <address class="has-text-primary" style="user-select: text">
 				<?= nl2br( get_theme_mod( 'address' )["postal"] ) ?>
             </address>
@@ -28,7 +28,7 @@ defined( 'ABSPATH' ) || exit;
         <?php endif; ?>
 	    <?php if (in_array("visitor", get_theme_mod("displayed_addresses") ?? [])): ?>
         <div class="is-flex-grow-1">
-            <h5 class="has-text-weight-semibold is-size-6 is-underlined"><?= esc_html__( 'Visitor Address', 'gegenlicht' ) ?></h5>
+            <h5 class="has-text-weight-semibold no-separator is-size-6 is-underlined"><?= esc_html__( 'Visitor Address', 'gegenlicht' ) ?></h5>
             <address class="has-text-primary" style="user-select: text">
 				<?= nl2br( get_theme_mod( 'address' )["postal"] ) ?>
             </address>
@@ -36,7 +36,7 @@ defined( 'ABSPATH' ) || exit;
 	    <?php endif; ?>
     </div>
     <hr class="separator is-primary"/>
-    <div class="has-text-primary my-2">
+    <div class="has-text-primary my-5">
         <h5 class="is-size-6 has-text-weight-semibold no-separator"><?= esc_html__( 'Youth Protection Officer', 'gegenlicht' ) ?></h5>
         <p><?= get_theme_mod( 'youth_protection_officer' )['name'] ?? 'MISSING' ?></p>
         <p><?= str_replace( "@", " (at) ", ( get_theme_mod( 'youth_protection_officer' )['email'] ?? "missing@missing" ) ) ?></p>
