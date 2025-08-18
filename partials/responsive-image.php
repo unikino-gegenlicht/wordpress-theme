a88dd67cc966f78cab07663e87c24c1dada1cc49
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-18 08:34:02 +0200

    chore: enhancing autoload of fallback image in responsive image

--- partials/responsive-image.php
+++ partials/responsive-image.php
@@ -5,12 +5,12 @@ $lazyLoad = (bool) ($args['lazyload'] ?? false);
 $style = ($args['style'] ?? "");
 
 if (isset($post) && $postID != null):
-    $desktopImageUrl = get_the_post_thumbnail_url( $postID, 'desktop' ) ?: wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'desktop' ) ?: wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'desktop' ) ?: "";
-    $mobileImageUrl = get_the_post_thumbnail_url( $postID, 'mobile' ) ?: wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'mobile' ) ?: wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'mobile' ) ?: "";
+    $desktopImageUrl = get_the_post_thumbnail_url( $postID, 'desktop' ) ?: wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'desktop' ) ?: "";
+    $mobileImageUrl = get_the_post_thumbnail_url( $postID, 'mobile' ) ?: wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'mobile' ) ?: "";
 	$alternativeDescription = (($imageID = attachment_url_to_postid($desktopImageUrl)) == 0) ? "" : get_post_meta($imageID, '_wp_attachment_image_alt', true);
 else:
-    $desktopImageUrl = $args['image_url'] ?? wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'desktop' ) ?: "";
-    $mobileImageUrl = $args['mobile_image_url'] ?? $args['image_url'] ?? wp_get_attachment_image_url( get_theme_mod( 'fallback_image' ), 'mobile' ) ?: "";
+    $desktopImageUrl = $args['image_url'] ?? wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'desktop' ) ?: "";
+    $mobileImageUrl = $args['mobile_image_url'] ?? $args['image_url'] ?? wp_get_attachment_image_url( get_theme_mod( 'anonymous_image' ), 'mobile' ) ?: "";
     $alternativeDescription = $args['alternative_description'] ?? "";
 endif;
 
