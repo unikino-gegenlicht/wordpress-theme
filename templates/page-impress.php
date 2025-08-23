b7c15a2ec2860e36ae058c43ca4acedfa6c51157
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-23 17:29:24 +0200

    chore: adding templates for contact pages and location pages

---
+++ templates/page-impress.php
@@ -0,0 +1,8 @@
+<main class="page-content">
+    <header>
+        <h1><?= esc_html__("Impress", "gegenlicht") ?></h1>
+    </header>
+    <div class="content impress pt-3 pb-5">
+        <?php the_content(); ?>
+    </div>
+</main>
---
+++ templates/page-location.php
@@ -0,0 +1,37 @@
+<article class="page-content content">
+    <figure class="image mb-3">
+        <picture>
+            <source srcset="<?= wp_get_attachment_image_url( get_theme_mod( "location_map" )["dark"] ) ?? "" ?>"
+                    media="(prefers-color-scheme: dark)">
+            <source srcset="<?= wp_get_attachment_image_url( get_theme_mod( "location_map" )["light"] ) ?? "" ?>">
+            <img src="<?= wp_get_attachment_image_url( get_theme_mod( "location_map" )["light"] ) ?? '' ?>"/>
+        </picture>
+    </figure>
+    <header>
+        <h1><?= get_the_title() ?></h1>
+    </header>
+	<?= apply_filters( 'the_content', get_the_content() ) ?>
+</article>
+<article class="mb-6"
+         style="--bulma-body-color: var(--bulma-primary); --bulma-body-background-color: black; color: var(--bulma-body-color); background-color: var(--bulma-body-background-color);">
+    <div class="page-content">
+		<?php if ( get_theme_mod( "location_unikum_logo" ) ): ?>
+            <figure class="image py-4">
+                <img style="max-height: 250px; object-fit: scale-down"
+                     src="<?= wp_get_attachment_image_url( get_theme_mod( "location_unikum_logo" ) ) ?? '' ?>"/>
+            </figure>
+		<?php endif; ?>
+        <h2><?= get_theme_mod( "location_unikum_block_title" )[ get_locale() ] ?? "" ?></h2>
+        <p class="pt-2"><?= get_theme_mod( "location_unikum_block_tagline" )[ get_locale() ] ?? "" ?></p>
+        <hr class="separator"/>
+        <div class="content pb-3">
+			<?= apply_filters( "the_content", get_theme_mod( "location_unikum_block_content" )[ get_locale() ] ?? "" ) ?>
+			<?php
+			get_template_part( 'partials/button', args: [
+				'href'     => get_theme_mod( "location_unikum_block_website_url" ),
+				'content'  => __( 'To the Website', 'gegenlicht' ),
+				'external' => true
+			] ); ?>
+        </div>
+    </div>
+</article>
