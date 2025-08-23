22d652caad540c2b8a61c5759d7544f3dd316682
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-23 17:31:15 +0200

    chore: disabling the semester break banner on the privacy policy page

--- privacy-policy.php
+++ privacy-policy.php
@@ -1,11 +1,11 @@
 <?php
 
 defined('ABSPATH') || exit;
-get_header();
+get_header( args: [ "hideBreakBanner" => true ] );
 ?>
 <main id="privacyPolicy" class="page-content">
 	<header>
-		<h1><?= esc_html__('Privacy Policy') ?></h1>
+		<h1><?= esc_html__('Privacy Policy', 'gegenlicht') ?></h1>
 	</header>
 	<div class="pt-3 content privacy-policy pb-5">
 		<?=	apply_filters('the_content', get_the_content()) ?>
