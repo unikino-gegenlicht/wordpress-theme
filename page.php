5176a2417f6b28d5d9325c1db7df26f092b1d0e2
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-23 17:29:45 +0200

    chore: adding loading of the newly introduced page templates

--- page.php
+++ page.php
@@ -1,48 +1,39 @@
 <?php
 defined( 'ABSPATH' ) || exit;
-get_header();
 do_action( 'wp_body_open' );
+
+if ( is_location_page() ) {
+	get_header();
+	get_template_part( 'templates/page', 'location' );
+	get_footer();
+
+	return;
+}
+
+if ( is_impress_page() ) {
+	get_header( args: [ "hideBreakBanner" => true ] );
+	get_template_part( 'templates/page', 'impress' );
+	get_footer();
+
+	return;
+}
+
+if ( is_contact_page() ) {
+	get_header();
+	get_template_part( 'templates/page', 'contact' );
+	get_footer();
+
+	return;
+}
+
+get_header();
 ?>
     <article class="page-content content">
-		<?php if ( is_location_page() ) : ?>
-            <figure class="image mb-3">
-                <picture>
-                    <source srcset="<?= wp_get_attachment_image_url( get_theme_mod( "location_map" )["dark"] ) ?? "" ?>"
-                            media="(prefers-color-scheme: dark)">
-                    <source srcset="<?= wp_get_attachment_image_url( get_theme_mod( "location_map" )["light"] ) ?? "" ?>">
-                    <img src="<?= wp_get_attachment_image_url( get_theme_mod( "location_map" )["light"] ) ?? '' ?>"/>
-                </picture>
-            </figure>
-		<?php endif; ?>
         <header>
             <h1><?= get_the_title() ?></h1>
         </header>
 		<?= apply_filters( 'the_content', get_the_content() ) ?>
     </article>
-<?php if ( is_location_page() ) : ?>
-    <article class="mb-6"
-            style="--bulma-body-color: var(--bulma-primary); --bulma-body-background-color: black; color: var(--bulma-body-color); background-color: var(--bulma-body-background-color);">
-        <div class="page-content">
-			<?php if ( get_theme_mod( "location_unikum_logo" ) ): ?>
-                <figure class="image py-4">
-                    <img style="max-height: 250px; object-fit: scale-down" src="<?= wp_get_attachment_image_url( get_theme_mod( "location_unikum_logo" ) ) ?? '' ?>"/>
-                </figure>
-			<?php endif; ?>
-            <h2><?= get_theme_mod( "location_unikum_block_title" )[ get_locale() ] ?? "" ?></h2>
-            <p class="pt-2"><?= get_theme_mod( "location_unikum_block_tagline" )[ get_locale() ] ?? "" ?></p>
-            <hr class="separator"/>
-            <div class="content pb-3">
-				<?= apply_filters( "the_content", get_theme_mod( "location_unikum_block_content" )[ get_locale() ] ?? "" ) ?>
-				<?php
-				get_template_part( 'partials/button', args: [
-					'href'     => get_theme_mod( "location_unikum_block_website_url" ),
-					'content'  => __( 'To the Website', 'gegenlicht' ),
-					'external' => true
-				] ); ?>
-            </div>
-        </div>
-    </article>
-<?php endif; ?>
 <?php
 get_footer();
 ?>
