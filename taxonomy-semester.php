11c5b77b7b5fdc924088bb5e913539afada54567
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-28 12:20:16 +0200

    chore: updating semester taxonomy page to display all movies screened in the semester

--- taxonomy-semester.php
+++ taxonomy-semester.php
@@ -2,50 +2,48 @@
 
 defined( 'ABSPATH' ) || exit;
 
+$taxonomy = get_queried_object();
 
-get_header();
+
+get_header( args: [ "title" => $taxonomy->name ] );
 do_action( 'wp_body_open' );
 
-$taxonomy = get_queried_object();
 ?>
 <main class="page-content">
-    <header>
-        <h1 class="is-size-2"><?= $taxonomy->name ?></h1>
-    </header>
-    <hr class="separator"/>
     <article>
-		<?php
-		$paragraphs = preg_split( "/\R\R/", $taxonomy->description );
-		foreach ( $paragraphs as $paragraph ) :
-			?>
-            <p>
-				<?= $paragraph ?>
-            </p>
-		<?php
-		endforeach;
-		?>
+        <header class="content">
+            <h1 class="is-size-2"><?= $taxonomy->name ?></h1>
+        </header>
+		<?= apply_filters( "the_content", $taxonomy->description ) ?>
     </article>
-	<?php if ( is_user_logged_in() ) : ?>
-        <article class="mt-6">
-            <header>
-                <h2 class="is-size-3"><?= esc_html__( 'The program hosted', 'gegenlicht' ) ?></h2>
-            </header>
-			<?php
-			$query = new WP_Query( array(
-				'post_type'      => [ 'movie' ],
-				'posts_per_page' => - 1,
-				'tax_query'      => array(
-					array(
-						'taxonomy' => 'special-program',
-						'terms'    => $taxonomy->term_id,
-					)
-				)
-			) );
-			while ( $query->have_posts() ) : $query->the_post();?>
-                <hr class="separator"/>
-                <p class="mt-2 has-text-weight-bold font-ggl is-uppercase is-size-5"><?= $post->post_title ?></p>
-			<?php endwhile; ?>
-            <hr class="separator"/>
-        </article>
-	<?php endif; ?>
+
+	<?php
+
+	$query = new WP_Query( array(
+		'post_type'      => [ 'movie', "event" ],
+		'posts_per_page' => - 1,
+		'tax_query'      => array(
+			array(
+				'taxonomy' => 'semester',
+				'terms'    => $taxonomy->term_id,
+			)
+		),
+		"meta_key"       => "screening_date",
+		"orderby"        => "meta_value_num",
+		"order"          => "ASC"
+	) );
+
+	while ( $query->have_posts() ): $query->the_post();
+		$screeningMonth                     = date( 'F', (int) rwmb_get_value( 'screening_date' ) );
+		$monthlyMovies[ $screeningMonth ][] = $post;
+	endwhile;
+
+	foreach ( $monthlyMovies as $month => $posts ) :
+		get_template_part( "partials/movie-list", args: [
+			"title" => esc_html__( $month, 'gegenlicht' ),
+			"posts" => $posts
+		] );
+    endforeach; ?>
+
+
 </main>
