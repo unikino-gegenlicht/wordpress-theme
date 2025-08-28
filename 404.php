493bf9da4e91e1d5d0f65e5b32f699041abffc09
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-08-28 12:19:54 +0200

    chore: created 404 page

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+defined("ABSPATH") || exit();
+
+get_header(args: ["title" => __("Not Found", "gegenlicht")]);
+?>
+<main class="page-content content mt-4">
+	<h1 class="has-text-centered"><?= __("Not Found", "gegenlicht")?></h1>
+	<?php
+	get_template_part( 'partials/responsive-image', args: [
+	'image_url'        => wp_get_attachment_image_url( get_theme_mod( 'semester_break_image' ), 'desktop' ),
+	'mobile_image_url' => wp_get_attachment_image_url( get_theme_mod( 'semester_break_image' ), 'mobile' ),
+	'fetch-priority'   => "high",
+	"loading"          => "eager"
+	] );
+	?>
+	<p class="mt-4"><?= esc_html__("The page you are trying to open could not be found. Please check the URL in your address bar", "gegenlicht") ?></p>
+	<?php get_template_part( 'partials/button', args: [
+		'href'    => "javascript:history.back()",
+		'content' => __( 'One Step Back', 'gegenlicht' ),
+		"external" => false,
+	] ) ?>
+	<?php get_template_part( 'partials/button', args: [
+		'href'    => get_home_url(scheme: "https"),
+		'content' => __( 'Back to the Frontpage', 'gegenlicht' )
+	] ) ?>
+</main>
