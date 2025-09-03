a896cac83e3eaccb37918082e99339e82e1e3a5f
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-09-03 18:59:39 +0200

    fix: using movie list part for intermission events

--- intermission.php
+++ intermission.php
@@ -45,40 +45,12 @@ if ( $query->have_posts() ):
     <div class="content pt-2">
 		<?= apply_filters( "the_content", get_theme_mod( "semester_break_pre_events_text" )[ get_locale() ] ?? "" ) ?>
     </div>
-    <div class="movie-list mb-5">
-        <p
-                class="font-ggl is-uppercase is-size-5 py-1 px-1"
-                style="background-color: var(--bulma-body-color); color: var(--bulma-body-background-color)">
-			<?= esc_html__( "Intermission Specials", "gegenlicht" ) ?>
-        </p>
-		<?php while ( $query->have_posts() ) : $query->the_post();
-			$showDetails = ( rwmb_meta( 'license_type' ) == 'full' || is_user_logged_in() );
-			$programType = rwmb_meta( 'program_type' );
-			if ( $programType == "special_program" ) {
-				$specialProgram = rwmb_meta( 'special_program' );
-			}
-			$title = $showDetails ? ( get_locale() == 'de' ? rwmb_meta( 'german_title' ) : rwmb_meta( 'english_title' ) ) : ( $programType == 'special_program' ? get_term( $specialProgram )->name : ( $post->post_type == "movie" ? esc_html__( 'An unnamed movie', 'gegenlicht' ) : esc_html__( 'An unnamed event', 'gegenlicht' ) ) );
-			?>
-            <a href="<?= get_permalink() ?>"
-               role="link"
-               aria-label="<?= $title ?>. <?= esc_html__( 'Screening starts: ', 'gegenlicht' ) ?> <?= date( "r", rwmb_meta( 'screening_date' ) ) ?>"
-            >
-                <div>
-                    <p class="is-size-6 m-0 p-0">
-                        <time datetime="<?= date( "Y-m-d H:i", rwmb_meta( 'screening_date' ) ) ?>">
-							<?= date( GGL_LIST_DATETIME, rwmb_meta( 'screening_date' ) ) ?>
-                        </time>
-                    </p>
-                    <h2 class="is-size-5 has-text-weight-bold is-uppercase no-separator"><?= $title ?></h2>
-                </div>
-
-                <span class="icon">
-                        <span class="material-symbols">arrow_forward_ios</span>
-                    </span>
-            </a>
-		<?php endwhile;
-		wp_reset_postdata(); ?>
-    </div>
+	<?php
+	get_template_part( 'partials/movie-list', args: [
+		"posts" => $query->posts,
+		"title" => esc_html__( "Intermission Specials", "gegenlicht" )
+	] );
+	?>
     <div class="content pt-2">
 		<?= apply_filters( "the_content", get_theme_mod( "semester_break_post_events_text" )[ get_locale() ] ?? "" ) ?>
 		<?= apply_filters( "the_content", get_theme_mod( "semester_break_text" )[ get_locale() ] ?? "" ) ?>
