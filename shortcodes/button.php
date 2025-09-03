09f168064497961a73740ba3d461e072584baff2
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2025-09-03 18:41:23 +0200

    fix: making all buttons hoverable, even those created by shortcodes

--- shortcodes/button.php
+++ shortcodes/button.php
@@ -4,11 +4,11 @@ function ggl_button_shortcode( $atts, $content = null ) {
 
 	$options = shortcode_atts( array(
 		'external' => false,
-        'backgroundColor' => 'var(--bulma-body-background-color)',
-        'textColor' => 'var(--bulma-body-color)',
+        'backgroundColor' => 'inherit',
+        'textColor' => 'inheritŝ',
         'link' => "",
 	), $atts );
-	return '<div class="py-1"><a class="button is-fullwidth is-uppercase is-size-5 is-outlined my-4" target="_blank" href="'.$options['link'].'" style="background-color: '. $options['backgroundColor'] .' !important; color: '. $options['textColor'] .' !important; ">
+	return '<div class="py-1"><a class="button is-fullwidth is-uppercase is-size-5 is-outlined my-4" target="_blank" href="'.$options['link'].'">
         <span class="has-text-weight-bold">'. $content .'</span>
         <span class="material-symbols ml-1">open_in_new</span>
     </a></div>';
