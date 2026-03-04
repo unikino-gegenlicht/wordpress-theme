01b9d396bce051b6c85b5add5927c1121e20804b
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2026-03-04 10:40:31 +0100

    chore: adding filter for overriding content display controls

--- src/filters.php
+++ src/filters.php
@@ -1,5 +1,5 @@
 <?php
-add_filter("ggl__show_full_details", "ggl_theme__default_show_full_details", priority: 1);
-function ggl_theme__default_show_full_details() : bool {
-	return (rwmb_get_value( "license_type" ) == "full") || is_user_logged_in();
+add_filter("ggl__show_full_details", "ggl_theme__default_show_full_details", priority: 1, accepted_args: 2);
+function ggl_theme__default_show_full_details(bool $anonymize, WP_Post $post) : bool {
+	return rwmb_get_value( "license_type", post_id: $post->ID ) == "full" || is_user_logged_in();
 }
