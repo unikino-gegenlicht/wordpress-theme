52137494ec607ce2eb2fb0a48b91aa38c60e2f20
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2026-03-29 12:56:16 +0200

    fix: loading post thumbnail correctly

--- page.php
+++ page.php
@@ -30,7 +30,13 @@ get_header();
 ?>
     <article class="page-content content mt-4">
         <header>
-            <?php if (has_post_thumbnail()) { ggl_the_post_thumbnail(); } ?>
+            <?php if (has_post_thumbnail()): ?>
+                <picture>
+                    <source srcset="<?= wp_get_attachment_image_srcset(get_post_thumbnail_id(), 'mobile') ?>" media="(width <= 768px)"/>
+                    <source srcset="<?= wp_get_attachment_image_srcset(get_post_thumbnail_id(), 'desktop') ?>" media="(width > 768px)"/>
+                    <img alt="" src="<?= get_the_post_thumbnail_url(size: 'desktop') ?>"
+                </picture>
+            <?php endif; ?>
             <h1 class="mt-2"><?= get_the_title() ?></h1>
         </header>
         <?= apply_filters( 'the_content', get_the_content() ) ?>
