c2799fe59a03207321c544fe6b8575a2a7f89857
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2026-08-25 14:42:32 +0200

    chore: adding single post page support

---
+++ single.php
@@ -0,0 +1,22 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+do_action( 'wp_body_open' );
+
+get_header();
+?>
+    <article class="page-content content mt-4">
+        <header>
+            <?php if (has_post_thumbnail()): ?>
+                <picture>
+                    <source srcset="<?= wp_get_attachment_image_srcset(get_post_thumbnail_id(), 'mobile') ?>" media="(width <= 768px)"/>
+                    <source srcset="<?= wp_get_attachment_image_srcset(get_post_thumbnail_id(), 'desktop') ?>" media="(width > 768px)"/>
+                    <img alt="" src="<?= get_the_post_thumbnail_url(size: 'desktop') ?>"
+                </picture>
+            <?php endif; ?>
+            <h1 class="mt-2"><?= get_the_title() ?></h1>
+        </header>
+        <?= apply_filters( 'the_content', get_the_content() ) ?>
+    </article>
+<?php
+get_footer();
+?>
