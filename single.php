a73f45c0161a45dde5e50b7ab9c8b8fe0af4591d
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2026-08-25 15:07:05 +0200

    chore: updating translations

--- single.php
+++ single.php
@@ -15,8 +15,8 @@ get_header();
             <?php endif; ?>
             <h1 class="mt-2"><?= get_the_title() ?></h1>
             <div class="has-text-right my-3">
-                <p class="m-0"><?= esc_html__('Published on') ?>:&nbsp;<?= get_post_datetime(field: 'date')->format(GGL_ENGLISH_DATETIME_FORMAT)?></p>
-                <p class="m-0"><?= esc_html__('Last edit') ?>:&nbsp;<?= get_post_datetime(field: 'modified')->format(GGL_ENGLISH_DATETIME_FORMAT)?></p>
+                <p class="m-0"><?= esc_html__('Published on', 'gegenlicht') ?>:&nbsp;<?= get_post_datetime(field: 'date')->format(str_starts_with( get_user_locale(), "de" ) ? GGL_THEME__GERMAN_DATETIME_FORMAT : GGL_THEME__ENGLISH_DATETIME_FORMAT)?></p>
+                <p class="m-0"><?= esc_html__('Last edit', 'gegenlicht') ?>:&nbsp;<?= get_post_datetime(field: 'modified')->format(str_starts_with( get_user_locale(), "de" ) ? GGL_THEME__GERMAN_DATETIME_FORMAT : GGL_THEME__ENGLISH_DATETIME_FORMAT)?></p>
             </div>
         </header>
         <?= apply_filters( 'the_content', get_the_content() ) ?>
